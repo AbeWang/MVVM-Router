28bc58bfd1b5e1bf7c7f42012e3d5c8d4921603f
Abe Wang <abewang@kkstream.com>
2021-03-07 22:33:38 +0800

    Adds new build function for FirstViewBuilder.

--- MVVM-Router/MVVM-Router/FirstViewController/Builder/FirstViewBuilder.swift
+++ MVVM-Router/MVVM-Router/FirstViewController/Builder/FirstViewBuilder.swift
@@ -5,13 +5,21 @@
 //  Created by Abe Wang on 2021/3/4.
 //
 
-import Foundation
+import UIKit
 
 class FirstViewBuilder: BuilderProtocol {
     static func build() -> FirstViewController {
         let viewController = FirstViewController()
         viewController.viewModel = FirstViewModel()
         viewController.router = FirstViewRouter(context: viewController)
+        return viewController
+    }
+}
+
+extension FirstViewBuilder {
+    static func build(backgroundColor: UIColor) -> FirstViewController {
+        let viewController = FirstViewBuilder.build()
+        viewController.view.backgroundColor = backgroundColor
         return viewController
     }
 }
